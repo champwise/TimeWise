67ac98e16a3d9ff1d89ca6186261b15387d9cdf1
Sam <samandrewmills@gmail.com>
2018-08-21 11:00:54 +1000

    ''

--- common/config.php
+++ common/config.php
@@ -2,7 +2,7 @@
 
     // Database credentials
 define('DB_HOST', 'timewise1-server.mysql.database.azure.com');
-define('DB_USER', 'champwise@timewise1-server');
+define('DB_USER', 'champwise');
 define('DB_PASS', 'jTel79rv');
 define('DB_NAME', 'timewisedb');
 
