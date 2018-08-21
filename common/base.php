ae3b71349ca8ada0bfdb842499e0a8f93ca0c2c8
Sam <samandrewmills@gmail.com>
2018-08-21 10:48:48 +1000

    ''

--- common/base.php
+++ common/base.php
@@ -11,28 +11,16 @@ include_once "config.php";
 
 
 
-<<<<<<< HEAD
     // Create a database object
 try {
-    $db = new PDO("sqlsrv:server = tcp:timewisesvr.database.windows.net,1433; Database = timewisedb", "champwise", 'jTel79rv');
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $dsn = "mysql:host=".DB_HOST.";dbname=".DB_NAME;
+    $db = new PDO($dsn, DB_USER, DB_PASS);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+    exit;
 }
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
-=======
-//     // Create a database object
-// try {
-//     $dsn = "mysql:host=".DB_HOST.";dbname=".DB_NAME;
-//     $db = new PDO($dsn, DB_USER, DB_PASS);
-// } catch (PDOException $e) {
-//     echo 'Connection failed: ' . $e->getMessage();
-//     exit;
-// }
 
-$con=mysqli_init();
- mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL);
- mysqli_real_connect($con, "timewise1-server.mysql.database.azure.com", "champwise@timewise1-server", {'jTel79rv'}, {"timewisedb"}, 3306);
->>>>>>> 807d07f... connection strin ghange
+// $con=mysqli_init();
+//  mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL);
+//  mysqli_real_connect($con, "timewise1-server.mysql.database.azure.com", "champwise@timewise1-server", {'jTel79rv'}, {"timewisedb"}, 3306);
 ?>
